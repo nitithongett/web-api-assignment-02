f138800ba57b88163874db68e371c0fa29f94b2d
Nitithon Kobtham <61160100@go.buu.ac.th>
2021-03-04 19:24:56 +0700

    create GET /books/:id to retrieve book data from array

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const app = express()
 app.use(express.json())
 let books = [] 
 
+app.get('/books', (req, res) => {
+    //input*
+
+    //process*
+
+    //output*
+    res.status(200).json(books)
+})
+
+
+app.get('/books/:id', (req, res) =>{
+    //input*
+    let id = req.params.id
+
+     let book = {} 
+
+    //process*
+    book = books[id] 
+
+    //output*
+    res.status(200).json(book)
+
+})
+
 app.post('/books', (req, res) => { 
 
     //input*
